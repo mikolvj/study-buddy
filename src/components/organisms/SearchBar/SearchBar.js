55d8c8c3c877902e3c48054fba19280fa3a1e020
mikolvj <mikolajborowskiii@gmail.com>
2022-10-26 19:29:25 +0200

    searchbar made with downshift working

--- src/components/organisms/SearchBar/SearchBar.js
+++ src/components/organisms/SearchBar/SearchBar.js
@@ -1,22 +1,30 @@
 import classes from './SearchBar.module.scss';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import debounce from 'lodash.debounce';
 import { useStudents } from '../../../hooks/useStudents';
+import { useCombobox } from 'downshift';
 
 const SearchBar = () => {
-	const [searchPhrase, setSearchPhrase] = useState('');
-	const [matchingStudents, setMatchingStudents] = useState('');
+	const [matchingStudents, setMatchingStudents] = useState([]);
 	const { findStudents } = useStudents();
 
-	const getMatchingStudents = debounce(async (e) => {
-		const { students } = await findStudents(searchPhrase);
+	const getMatchingStudents = debounce(async ({ inputValue }) => {
+		const { students } = await findStudents(inputValue);
 		setMatchingStudents(students);
 	}, 500);
 
-	useEffect(() => {
-		if (!searchPhrase) return;
-		getMatchingStudents(searchPhrase);
-	}, [searchPhrase, getMatchingStudents]);
+	// Downshift hook
+	const {
+		isOpen,
+		getMenuProps,
+		getInputProps,
+		getComboboxProps,
+		highlightedIndex,
+		getItemProps,
+	} = useCombobox({
+		items: matchingStudents,
+		onInputValueChange: getMatchingStudents,
+	});
 
 	return (
 		<div className={classes.wrapper}>
@@ -24,21 +32,31 @@ const SearchBar = () => {
 				<h4>Logged as:</h4>
 				<h3>Teacher</h3>
 			</div>
-			<div className={classes.searchWrapper}>
-			<input
-				className={classes.searchInput}
-				placeholder='Search'
-				onChange={(e) => setSearchPhrase(e.target.value)}
-				value={searchPhrase}
-				name='Search'
-			></input>
-			{searchPhrase && matchingStudents.length ? (
-				<ul className={classes.searchList}>
-					{matchingStudents.map((student) => (
-						<li className={classes.searchItem}key={student.id}>{student.name}</li>
-					))}
+			<div {...getComboboxProps} className={classes.searchWrapper}>
+				<input
+					className={classes.searchInput}
+					placeholder='Search'
+					{...getInputProps()}
+					name='Search'
+					id='Search'
+				></input>
+				<ul className={classes.searchList} {...getMenuProps()}>
+					{isOpen &&
+						matchingStudents.map((item, index) => (
+							<li
+								{...getItemProps({ item, index })}
+								key={item.id}
+								className={
+									highlightedIndex === index
+										? `${classes.high} + ${classes.searchItem}`
+										: classes.searchItem
+								}
+							>
+								{item.name}
+							</li>
+						))}
 				</ul>
-			) : null}</div>
+			</div>
 		</div>
 	);
 };
--- src/views/Root.js
+++ src/views/Root.js
@@ -7,7 +7,6 @@ import {
 import MainTemplate from '../components/templates/MainTemplate';
 import Dashboard from './Dashboard';
 import AddUserView from './AddUserView';
-// import UsersProvider from '../providers/UsersProvider';
 
 import '../assets/styles/global.scss';
 
@@ -15,16 +14,13 @@ function Root() {
 	return (
 		<Router>
 			<MainTemplate>
-				{/* <UsersProvider> */}
 				<Routes>
 					<Route path='/' exact element={<Navigate to='/dashboard' />} />
-					{/* <Route path='/dashboard/:id?' exact element={<Dashboard />} /> */}
 					<Route path='/dashboard/' element={<Dashboard />}>
 						<Route path=':groupId' element={<Dashboard />} />
 					</Route>
 					<Route path='/add-user' element={<AddUserView />} />
 				</Routes>
-				{/* </UsersProvider> */}
 			</MainTemplate>
 		</Router>
 	);
